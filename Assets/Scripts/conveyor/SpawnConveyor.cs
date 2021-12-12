7cfc5a69d4c850d0383e66a04bec817cd2c0eb3b
Maks <75748966+MaksErof@users.noreply.github.com>
2021-12-12 16:45:58 +0300

    second task

---
+++ Assets/Scripts/conveyor/SpawnConveyor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnConveyor : MonoBehaviour
+{
+    [SerializeField] private Conveyor _template;
+    [SerializeField] private Transform _spawnPoint;
+    bool isTouch = false;
+
+    private void Start()
+    {
+        CreateConveyor(_template,_spawnPoint);
+    }
+
+    public void CreateConveyor(Conveyor _template, Transform spawnPoint)
+    {
+        Vector3 spawnPosition = new Vector3(0,0,1);
+
+        for(int i = 0; i < 20; i ++)
+        {
+            Instantiate(_template, spawnPoint.position, Quaternion.Euler(0, 0, 90));
+            spawnPoint.position += spawnPosition;
+        }
+    }
+}
+
---
+++ Assets/Scripts/wall/SpawnWall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWall : MonoBehaviour
+{
+    [SerializeField] private Wall _template;
+    [SerializeField] private Transform[] _pointInstalations;
+
+    private void Start()
+    {
+        for(int i = 0; i < _pointInstalations.Length; i++)
+        {
+            Instantiate(_template, _pointInstalations[i].position, Quaternion.identity);
+        }
+        
+    }
+}
