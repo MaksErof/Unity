28933b51f20fc01066b470a378a965cd8bcd6cb8
Maks <75748966+MaksErof@users.noreply.github.com>
2022-02-04 17:42:58 +0300

    third task

---
+++ bowling/Assets/Scripts/BallMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallMove : MonoBehaviour
+{
+    [SerializeField] private float _ballSpeed;
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody _rigidbody;
+    private float _move;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out Skittles skittles))
+        {
+            _rigidbody.AddForce(Vector3.zero);
+        }
+    }
+
+    private void Update()
+    {
+        MoveBall();
+        SetForce(_ballSpeed);
+    }
+
+    private void MoveBall()
+    {
+        _move = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+        transform.Translate(_move,0,0);
+    }
+
+    private void SetForce(float ballSpeed)
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.forward * ballSpeed, ForceMode.Impulse);
+        }       
+    }
+}
---
+++ bowling/Assets/Scripts/SkittlesBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkittlesBuilder : MonoBehaviour
+{
+    [SerializeField] private float _numberOfRows;
+    [SerializeField] private Skittles _skittles;
+    [SerializeField] private Transform _builderPoint;
+
+    private void Start()
+    {
+        Instantiate(_skittles, _builderPoint);
+    }
+}
