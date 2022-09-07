c712f35c84864696e2a5dceb0cf896d97f891db9
Maks <75748966+MaksErof@users.noreply.github.com>
2022-09-07 17:53:36 +0300

    second task

--- Conveyor1/Assets/Scripts/Conveyor.cs
+++ Conveyor1/Assets/Scripts/Conveyor.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-
 public class Conveyor : MonoBehaviour
 {
-    [SerializeField] private Transform _endPosition;
-    [SerializeField] private float _conveyorSpeed;
-
-    private void OnTriggerStay(Collider other)
-    {
-        other.transform.position = Vector3.MoveTowards(other.transform.position, _endPosition.position, _conveyorSpeed * Time.deltaTime);
-    }
 }
---
+++ Conveyor1/Assets/Scripts/MovingOfObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class MovingOfObjects : MonoBehaviour
+{
+    [SerializeField] private float _objectSpeed;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void MoveAround()
+    {
+        _rigidbody.AddForce(Vector3.left * _objectSpeed);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        MoveAround();
+    }
+}
